58220b5b8eac6ec3c21c21813af87f0d96391a6c
Tom Langwaldt <t.langwaldt@gmail.com>
2024-01-26 20:19:15 +0100

    Add environment interactions

---
+++ godot/interactions/gravity_fields/gravity_field.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+func _on_body_entered(body):
+	body.react_to_gravity(Vector2(0, 1), 980)
+
+
+func _on_body_exited(body):
+	if body.has_method("react_to_gravity"):
+		body.react_to_gravity(Vector2(0, 1), 0)
