71d6977a38e4730a9e2d868ee8eb858cdd7d898f
JoeyGartain <joey.gartain@gmail.com>
2024-01-27 15:45:45 +0100

    Add elevator out of order

---
+++ godot/scenes/level_1.gd
@@ -0,0 +1,9 @@
+extends Node2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var current_animation_name = "elevator_movement"
+	#animation_player.play(current_animation_name)
+	#var animation = animation_player.get_animation(current_animation_name)
+	#animation.loop_mode = Animation.LOOP_LINEAR
