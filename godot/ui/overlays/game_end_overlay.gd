59bf50fb2e32a7b947344cd4e846f840e8fa4a61
Tom Langwaldt <t.langwaldt@gmail.com>
2024-01-28 14:34:52 +0100

    Add things

--- godot/ui/overlays/game_end_overlay.gd
+++ godot/ui/overlays/game_end_overlay.gd
@@ -2,6 +2,8 @@ extends Control
 
 func set_winner(player_id: int):
 	$PlayerName.text = "Player " + str(player_id + 1) + " won!"
+	$AudioStreamPlayerWinne.play()
+	
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_menu_scene.tscn")
