84445e0dc5d3144200340fadd9bca7ddf3acd9d4
JoeyGartain <joey.gartain@gmail.com>
2024-01-28 09:57:56 +0100

    Add elevator

--- godot/scenes/level_1.gd
+++ godot/scenes/level_1.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#var current_animation_name = "elevator_movement"
-	#animation_player.play(current_animation_name)
-	#var animation = animation_player.get_animation(current_animation_name)
-	#animation.loop_mode = Animation.LOOP_LINEAR
+	var current_animation_name = "elevator_movement"
+	animation_player.play(current_animation_name)
+	var animation = animation_player.get_animation(current_animation_name)
+	animation.loop_mode = Animation.LOOP_LINEAR
